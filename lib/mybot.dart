6d4f2da297e4afe344c13c85d3766298e2341606
Suhaibusman <suhaibusman54@gmail.com>
2024-01-27 18:45:33 +0500

    updated

--- lib/mybot.dart
+++ lib/mybot.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'dart:convert';
 
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -73,9 +76,18 @@ class _ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
+        key: _scaffoldKey,
         appBar: AppBar(
-          leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+          leading: IconButton(
+              onPressed: () {
+                _scaffoldKey.currentState!.openDrawer();
+              },
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.white,
+              )),
           title: const Text(
             'Gemini AI',
             style: TextStyle(
@@ -90,6 +102,117 @@ class _ChatScreenState extends State<ChatScreen> {
             166,
             126,
             1,
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(
+                    0,
+                    166,
+                    126,
+                    1,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: CircleAvatar(
+                        radius: 40,
+                        backgroundImage: NetworkImage(
+                          'https://media.licdn.com/dms/image/D5603AQEm8gqDfvzrBA/profile-displayphoto-shrink_800_800/0/1689103165548?e=2147483647&v=beta&t=g7TaydPxRthr2NDcvMNIKXHSoi_r2UvYz8FYKz3BuFI',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Center(
+                      child: Text(
+                        'Developed By Muhammad Suhaib',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://img.icons8.com/color/48/000000/portfolio.png', // Portfolio icon
+                  height: 24,
+                  width: 24,
+                ),
+                title: const Text('Click to See Portfolio'),
+                onTap: () {
+                  // Handle portfolio tap
+                  launch('https://suhaibportfolio.vercel.app/');
+                },
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://img.icons8.com/color/48/000000/gmail.png', // Gmail icon
+                  height: 24,
+                  width: 24,
+                ),
+                title: const Text('suhaibusman54@gmail.com'),
+                onTap: () {
+                  launch('mailto:suhaibusman54@gmail.com');
+                  // Handle email tap
+                },
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://img.icons8.com/color/48/000000/whatsapp.png', // WhatsApp icon
+                  height: 24,
+                  width: 24,
+                ),
+                title: const Text(' 03112136120'),
+                onTap: () {
+                  launch(
+                      'https://api.whatsapp.com/send?phone=+923112136120&text=Hello,%20Suhaib%20See%20Your%20GeminiAiApp');
+                  // Handle contact tap
+                },
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://img.icons8.com/color/48/000000/linkedin.png', // LinkedIn icon
+                  height: 24,
+                  width: 24,
+                ),
+                title: const Text('Muhammad Suhaib Usman'),
+                onTap: () {
+                  launch('https://www.linkedin.com/in/suhaibusman/');
+                },
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://img.icons8.com/color/48/000000/instagram.png', // Instagram icon
+                  height: 24,
+                  width: 24,
+                ),
+                title: const Text('suhaib__usman'),
+                onTap: () {
+                  launch('https://instagram.com/suhaib__usman');
+                },
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://img.icons8.com/color/48/000000/facebook-new.png', // Facebook icon
+                  height: 24,
+                  width: 24,
+                ),
+                title: const Text('Muhammad Suhaib'),
+                onTap: () {
+                  launch('https://www.facebook.com/MuhammadSuhaib0/');
+                },
+              ),
+            ],
           ),
         ),
         body: DashChat(
